1d4092ef6b2c690387166502acdbf195dfd08ca0
Vlad-UA-KR <vlad.fedorchenkog@gmail.com>
2022-09-27 15:08:12 +0300

    change menu

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,21 @@
-$(document).ready(function () {
-	$('.header__btn').click(function (event) {
-		$('.header__btn,.header__body').toggleClass('active')
-		$('body').toggleClass('lock');
-	});
+// $(document).ready(function () {
+// 	$('.header__btn').click(function (event) {
+// 		$('.header__btn,.header__body').toggleClass('active')
+// 		$('body').toggleClass('lock');
+// 	});
+// });
+
+$(function () {
+
+	$('body').click(function (e) {
+
+		if ($(e.target.closest('i')).hasClass('header__btn')) {
+			$('.header__btn,.header__body').toggleClass('active');
+			$('body').toggleClass('lock');
+			return
+		};
+		console.log('da');
+		$('.header__btn,.header__body').removeClass('active');
+		$('body').removeClass('lock');
+	})
 });
